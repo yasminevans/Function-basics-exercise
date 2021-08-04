e53bc360dd479083b91db856edcd8dff6c9c8f69
Yasmin <88096861+yasminevans@users.noreply.github.com>
2021-08-04 09:55:40 +0200

    Add files via upload

---
+++ Opdracht_FunctionBasicsExercise/script.js
@@ -0,0 +1,23 @@
+const makePizza = function () {
+    console.log("Maak deeg");
+    console.log("Laat deeg rusten");
+    console.log("Voorverwarm de oven");
+    console.log("Maak saus");
+    console.log("Rol deeg uit");
+    console.log("Besmeer met saus, kaas en salami");
+    console.log("Knal in de oven");
+}
+
+const makeSushi = function () {
+    console.log("Kook de rijst");
+    console.log("Voeg rijstazijn toe en laat rijst 20 minuten afkoelen");
+    console.log("Snij alle groentes en vis in stukjes");
+    console.log("Rol de sushi's");
+    console.log("Snij in stukjes en serveer met sojasaus");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
